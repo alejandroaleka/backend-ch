c0017d187426c2d4bc0b7e80f4b06d71cb2ec948
Alejandro Fernandez <alejandro.aleka@gmail.com>
2024-03-10 19:56:08 -0300

    entrega 01 terminada

--- src/productManager.js
+++ src/productManager.js
@@ -1,31 +1,64 @@
 //clase ProductManager
 class ProductManager {
+    #products;
+    static productId = 0;
     constructor(){
-        this.products = []; //Se inicializa el arreglo vacío
+        this.#products = []; //Se inicializa el arreglo vacío
     }
 
     //Método para agregar productos
-    addProduct(title, description, price, thumbnail, code, stock) {
-        const newProduct = {
-            id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        };
-        this.products.push(newProduct);
+    addProduct(data) {
+        if (
+            !data.title || 
+            !data.description || 
+            !data.price || 
+            !data.thumbnail || 
+            !data.code || 
+            !data.stock
+        ) {
+            return "Error: Todos los campos son obligatorios. \n";
+        }
+
+        const codeExistence = this.#products.some(p => p.code === data.code);
+
+        const currentIndex = this.#products;
+
+        if (codeExistence) {
+            return `El código de producto ${data.code} ya fue utilizado. \n`;
+          } else {
+            const newProduct = {
+              id: currentIndex.length + 1,
+              title: data.title,
+              description: data.description,
+              price: data.price,
+              thumbnail: data.thumbnail,
+              code: data.code,
+              stock: data.stock
+            }
+            this.#products.push(newProduct);
+            return `Producto agregado con éxito. \n`;
+        };       
     }
 
-    //Método para retornar el array de productos
+    //Consulta de todos los productos
     getProducts() {
-        return this.products;
+        return this.#products;
     }
 
-    //Método para retornar un producto a partir de un Id
+    //Consulta de un producto determinado por su Id
     getProductById(id) {
-
+        try {
+            const product = this.#products.find((p) => p.id === id);
+            if(!product) {
+                throw new Error(`No se encontraron productos con el ID ${id}`);
+            }
+            return product;
+        } catch (err) {
+            console.error(err);
+        }
     }
+
 }
 
+
+module.exports = ProductManager;
---
+++ src/testing.js
@@ -0,0 +1,37 @@
+const ProductManager = require("./productManager");
+
+const producto = new ProductManager();
+
+console.clear();
+
+console.log(producto.getProducts());
+
+const data = {
+    title: "producto prueba",
+    description: "Este es un producto de prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abc123",
+    stock: 25
+}
+
+const data2 = {
+    title: "otro prod",
+    description: "Este es un producto de prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: "abcasdfas123",
+    stock: 25
+}
+
+console.log(producto.addProduct(data));
+console.log(producto.addProduct(data2));
+
+
+console.log(producto.getProducts());
+
+console.log(producto.getProductById(1));
+
+console.log(producto.addProduct(data));
+
+console.log(producto.getProductById(3));
